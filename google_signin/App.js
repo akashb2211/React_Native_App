01505035a80f2e391bffb7c4f0dd3bfae53dce6f
AKASH BHOSALE <142890463+akashb2211@users.noreply.github.com>
2024-04-25 14:07:13 +0530

    Add files via upload

---
+++ google_signin/App.js
@@ -0,0 +1,120 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+WebBrowser.maybeCompleteAuthSession();
+
+
+//web: 111668913685-gik6bmkp08eefeo82a330phkd9efbdbp.apps.googleusercontent.com
+//ios:111668913685-1oborlrg4m2ngietofvefppk79mcmsqu.apps.googleusercontent.com
+//Android: 111668913685-ojrs4b8t3gvqknfp80n1qmg9a39qd2kb.apps.googleusercontent.com 
+
+
+export default function App() {
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "111668913685-ojrs4b8t3gvqknfp80n1qmg9a39qd2kb.apps.googleusercontent.com",
+    iosClientId: "111668913685-1oborlrg4m2ngietofvefppk79mcmsqu.apps.googleusercontent.com",
+    webClientId: "111668913685-gik6bmkp08eefeo82a330phkd9efbdbp.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    handleEffect();
+  }, [response, token]);
+
+  async function handleEffect() {
+    const user = await getLocalUser();
+    console.log("user", user);
+    if (!user) {
+      if (response?.type === "success") {
+        // setToken(response.authentication.accessToken);
+        getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(user);
+      console.log("loaded locally");
+    }
+  }
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {!userInfo ? (
+        <Button
+          title="Sign in with Google"
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      ) : (
+        <View style={styles.card}>
+          {userInfo?.picture && (
+            <Image source={{ uri: userInfo?.picture }} style={styles.image} />
+          )}
+          <Text style={styles.text}>Email: {userInfo.email}</Text>
+          <Text style={styles.text}>
+            Verified: {userInfo.verified_email ? "yes" : "no"}
+          </Text>
+          <Text style={styles.text}>Name: {userInfo.name}</Text>
+          {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
+        </View>
+      )}
+      <Button
+        title="remove local store"
+        onPress={async () => await AsyncStorage.removeItem("@user")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  card: {
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 15,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
